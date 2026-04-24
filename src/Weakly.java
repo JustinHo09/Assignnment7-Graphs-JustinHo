5223e9ca6462805ff7c0ec63e1330118dc43e5cc
JustinHo09 <hojustin09@gmail.com>
2026-04-23 21:33:42 -0500

    I got started on problem 4, made the main method and started on the isWeakly method.
    I got started on trying to solve problem 4 for the Graphs assignment. I first made the main method which will run a test and print the result on if it is weakly connected or not. Then I got stared on the isWeakly method by just making hte header.

--- src/Weakly.java
+++ src/Weakly.java
@@ -1,2 +1,13 @@
 public class Weakly {
+
+    public static void main(String[] args){
+        int [][] matrix = {{0,1,0},
+                           {0,0,0},
+                            {0,1,0}};
+        System.out.println("Weakly Connected: "+ isWeakly(matrix));
+    }
+
+    public static boolean isWeakly(int [][] adjMatrix){
+        return false;
+    }
 }
