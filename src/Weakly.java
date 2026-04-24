0610636cbc8e817b8999ecc314240e5048543a80
JustinHo09 <hojustin09@gmail.com>
2026-04-23 21:48:43 -0500

    I got started on the logic of the isWeakly method.
    I got started on the logic of the is weakly method. I decided to implemennt a DFS traversal of this matrix. I have a stack to keep track of verticies to visit and a boolean array to keep track of visited becuase the matrix already gives us an index corresponding to the vertex which can be used in the array. Then I got the end logic to check that if the count after the traversal is equal to n meaning it's connect and if not that it's not connected.

--- src/Weakly.java
+++ src/Weakly.java
@@ -1,3 +1,4 @@
+import java.util.Stack;
 public class Weakly {
 
     public static void main(String[] args){
@@ -8,6 +9,31 @@ public class Weakly {
     }
 
     public static boolean isWeakly(int [][] adjMatrix){
+
+        int n = adjMatrix.length;
+        if(n<=1){
+            return true;
+        }
+
+        Stack<Integer> vertecies;
+        boolean [] visited = new boolean[n];
+
+        // Count how many nodes are visited, should be all
+        int count=0;
+
+        // Do a DFS traversal
+
+
+
+        // If all nodes are visited its connected and at least weakly
+        if(count == n){
+            return true;
+        }
+
         return false;
+    }
+
+    public static void traversal(int startIndex,int[][] adjMatrix, boolean[] visited){
+
     }
 }
