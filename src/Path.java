a8426eb2f3a4d6d9d55e8a5c51e2ee0ed684d815
JustinHo09 <hojustin09@gmail.com>
2026-04-24 22:01:11 -0500

    I added a test case in the main method, and finished implementing the path method.
    I added a test case with a cycle from 0,1,2,3,0. Then I finished implementing the path checking with four for loops. Each one will check a row and column in the matrix until it finds a non 0 value, indicating a directed edge. Then it does that for every vertex, but it will also make sure that the same node is not being accounted for twice usign it's index. Once four unique indexe are found, if there is an edge between on the row of index v4 at column idnex v1, then that means there is a loop from the end to begining that is length 4.

--- src/Path.java
+++ src/Path.java
@@ -1,10 +1,10 @@
 public class Path {
 
     public static void main(String[] args){
-        int[][] input = {{},
-                         {},
-                         {},
-                         {}
+        int[][] input = {{0,1,0,0},
+                         {0,0,1,0},
+                         {0,0,0,1},
+                         {1,0,0,0}
         };
         printCycle(input);
     }
@@ -14,15 +14,36 @@ public class Path {
             System.out.println("No cycle length of 4 exists");
             return;
         }
+        int n  = matrix.length;
         // For all conditions it cannot point to itself otherwise length != 4 for that path,
         // Furthurmore for this path they cannot point to any previous 1, otherwise
         // it would just be a shorter path.
-        // Check if the vertex 1 points to vertex 2
+        for(int v1 = 0; v1 < n; v1++){
+            for(int v2 = 0; v2 < n; v2++){
+                // Check if the vertex 1 points to vertex 2
+                if(v1 != v2 && matrix[v1][v2] != 0){
+                    for(int v3 = 0; v3 < n; v3++){
+                        // Check if vertex 2 points to vertex 3
+                        if(v3 != v2 && v3!= v1 && matrix[v2][v3] != 0){
+                            for(int v4 = 0; v4 < n; v4 ++){
+                                // check if vertex 3 points to vertex 4
+                                if(v4 != v3 &&  v4 != v2 && v4 != v1 && matrix[v3][v4] != 0){
+                                    //check if vertex 4 points to vertex 1
+                                    if(matrix[v4][v1] != 0){
+                                        System.out.println("Cycle(vertice's index): " + v1 + " -> " + v2 +
+                                                " -> " + v3 + " -> " + v4 + " -> " + v1);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-        // Check if vertex 2 points to vertex 3
 
-        // check if vertex 3 points to vertex 4
 
-        //check if vertex 4 points to vertex 1
+
+
     }
 }
