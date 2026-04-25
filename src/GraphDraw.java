ef8be29c502db9c3d09a50b8b3a1d0228d28ab27
JustinHo09 <hojustin09@gmail.com>
2026-04-24 23:29:26 -0500

    I changed the way I found the vertex names, and I added comments to find time and space.
    I changed thee way I found and made the vertex name array by finding andd building it manuallly. I do this first by finding how many spaces there are since that +1 gives how many wordsd there are. Then I loop theough the string and concatinate each charaters to the currentstring until a space is reached where that string is addedd to the array and tehn thee current string is reset to "". The reason I even do this is becuase I do not beleive I would be able to explain the .split() method's time and space complexity sufficiently enough for problem 7. I allso added some comments to help understand what the the time and space complexity is.

--- src/GraphDraw.java
+++ src/GraphDraw.java
@@ -10,14 +10,38 @@ public class GraphDraw {
             return;
         }
 
+        // Find how many words there are by finding how many spaces there are
+        int spaceCount=0;
+        for(int i =0; i<s.length();i++){
+            if(s.charAt(i) == ' '){
+                spaceCount++;
+            }
+        }
+        //O(n)
         // Fill array with each string separated by space
-        String[] verticies = s.split(" ");
+        int n = spaceCount+1;
+        String[] verticies = new String[n];
+        // Fill the verticies with the names and build the names
+        // O(k^2) S(k)
+        int index = 0;
+        String current = "";
+        for(int i=0; i< s.length();i++){
+            if(s.charAt(i) != ' '){
+                current += s.charAt(i);
+            }else{
+                verticies[index] = current;
+                current = "";
+                index++;
+            }
+        }
+        verticies[index] = current;
         int left;
         int right;
 
-        int n = verticies.length;
+        // O and S(n^2)
         int[][] adjMatrix = new int[n][n];
 
+        // O(n)
         for( int i =0; i < n; i++){
             // calculate its left edge
             left = ((2*i)+2) % n;
@@ -27,13 +51,13 @@ public class GraphDraw {
             adjMatrix[i][right] = 1;
         }
 
-
+        //O(n)
         for(int i=0; i< n; i++){
             System.out.print("\t\t" + verticies[i]);
         }
 
         System.out.print("\n");
-        
+        // O(n^2)
         for(int i=0; i<n;i++){
             System.out.print(verticies[i]);
             for(int j=0;j<n;j++){
