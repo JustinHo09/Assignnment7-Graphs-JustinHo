2de2ce347f85fef2d7326d5c8a061668fadbba53
JustinHo09 <hojustin09@gmail.com>
2026-04-24 22:46:36 -0500

    I finished the Draw Graph method but its adjacency matrix output.
    I finished the draw graph method's calulation of it's right and left for the adjacency matrix, and made the matrix. Then I printed out the correcsponding matrxi with a loop, but the formattinng only works if they are all the same length or three charcters.

--- src/GraphDraw.java
+++ src/GraphDraw.java
@@ -1,2 +1,45 @@
 public class GraphDraw {
+
+    public static void main(String[] args){
+        String input = "ANT CUN BOG AMA DC TOL SAN";
+        draw(input);
+    }
+
+    public static void draw(String s){
+        if(s == null || s.equals("")){
+            return;
+        }
+
+        // Fill array with each string separated by space
+        String[] verticies = s.split(" ");
+        int left;
+        int right;
+
+        int n = verticies.length;
+        int[][] adjMatrix = new int[n][n];
+
+        for( int i =0; i < n; i++){
+            // calculate its left edge
+            left = ((2*i)+2) % n;
+            adjMatrix[i][left] = 1;
+            // calculate its right edge
+            right = ((2*i)+1) % n;
+            adjMatrix[i][right] = 1;
+        }
+
+
+        for(int i=0; i< n; i++){
+            System.out.print("\t\t" + verticies[i]);
+        }
+
+        System.out.print("\n");
+        
+        for(int i=0; i<n;i++){
+            System.out.print(verticies[i]);
+            for(int j=0;j<n;j++){
+                System.out.print("\t\t"+adjMatrix[i][j]);
+            }
+            System.out.print("\n");
+        }
+    }
 }
