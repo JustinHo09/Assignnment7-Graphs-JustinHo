e1c9bdfcc0fd1d7c81b7f0f84f12abf5a274d374
JustinHo09 <hojustin09@gmail.com>
2026-04-24 21:46:41 -0500

    I made a main method to test my algorithm and made the header for it
    I made the main method, which will be used to test the program. Then I got started oon the method header for the print cycle method. This will first check if the matrix is null or is the length is less than 4. This is becuase if it does not have atleas 4 elements in it, then no cycle length of 4 can exist. I also added comments to help me get started on the logic of what I need to do.

--- src/Path.java
+++ src/Path.java
@@ -1,4 +1,28 @@
 public class Path {
 
-    
+    public static void main(String[] args){
+        int[][] input = {{},
+                         {},
+                         {},
+                         {}
+        };
+        printCycle(input);
+    }
+
+    public static void printCycle(int[][] matrix){
+        if(matrix == null || matrix.length < 4){
+            System.out.println("No cycle length of 4 exists");
+            return;
+        }
+        // For all conditions it cannot point to itself otherwise length != 4 for that path,
+        // Furthurmore for this path they cannot point to any previous 1, otherwise
+        // it would just be a shorter path.
+        // Check if the vertex 1 points to vertex 2
+
+        // Check if vertex 2 points to vertex 3
+
+        // check if vertex 3 points to vertex 4
+
+        //check if vertex 4 points to vertex 1
+    }
 }
